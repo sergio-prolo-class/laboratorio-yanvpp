d3dbd91479fbe3abd4bef76927d66d079c4e42b5
Yan Vitor Pires Pereira <yanvitor.pp@gmail.com>
2025-07-04 11:20:10 -0300

    Treino

--- PROG1/aulas/Exercícios para treino - C/corretagem.c
+++ PROG1/aulas/Exercícios para treino - C/corretagem.c
@@ -7,17 +7,17 @@ int main(){
     scanf("%f", &valor_negociado);
 
     if (valor_negociado<2500.0){
-        comissao = 30.0 + (valor_negociado * 1.7);
+        comissao = 30.0 + (valor_negociado * 0.017);
     } else if (valor_negociado >= 2500.0 && valor_negociado < 6250.0){
-        comissao = 56.0 + (valor_negociado * 0.66);
+        comissao = 56.0 + (valor_negociado * 0.0066);
     }else if (valor_negociado >= 6250.0 && valor_negociado < 20000.0){
-        comissao = 76.0 + (valor_negociado*0.34);
+        comissao = 76.0 + (valor_negociado*0.0034);
     }else if (valor_negociado >= 20000 && valor_negociado < 50000.0){
-        comissao = 100.0 + (valor_negociado * 0.22);
+        comissao = 100.0 + (valor_negociado * 0.0022);
     }else if (valor_negociado >= 50000.0 && valor_negociado < 500000.0){
-        comissao = 155.0 + (valor_negociado * 0.11);
+        comissao = 155.0 + (valor_negociado * 0.0011);
     }else{
-        comissao = 255.0 + (valor_negociado * 0.09);
+        comissao = 255.0 + (valor_negociado * 0.0009);
     }
 
     if (comissao < 39.0){
