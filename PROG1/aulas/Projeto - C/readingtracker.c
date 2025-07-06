2227f6349056b02f464f3ca9631b2364a9dd47f1
Yan Vitor Pires Pereira <yanvitor.pp@gmail.com>
2025-07-06 18:31:24 -0300

    Atualização

--- PROG1/aulas/Projeto - C/readingtracker.c
+++ PROG1/aulas/Projeto - C/readingtracker.c
@@ -2,10 +2,15 @@
 #include <string.h>
 #include <stdbool.h>
 
-
+typedef struct {
+    char titulo[50];
+    char autor[50];
+    int data_inicio;
+    int data_fim;
+    char comentario[1000];
+} Livro;
 
 int main()
 {
-
     return 0;
 }
