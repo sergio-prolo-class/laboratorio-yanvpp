26c14f15cf0486bc8cf9856152caecdb3b811711
Yan Vitor Pires Pereira <yanvitor.pp@gmail.com>
2025-07-06 18:30:46 -0300

    Treino de domingo

--- PROG1/aulas/Exercícios para treino - C/teste.c
+++ PROG1/aulas/Exercícios para treino - C/teste.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+typedef struct {
+    int idade;
+    float altura;
+} Pessoa;
+
+void celebrar_aniversario(Pessoa *p)
+{
+    (*p).idade++;
+}
+
 int main()
 {
+    Pessoa prof = {40, 1.64};
+    printf("Idade antes: %d\n", prof.idade);
+    celebrar_aniversario(&prof);
+    printf("Idade depois: %d\n", prof.idade);
     return 0;
 }
