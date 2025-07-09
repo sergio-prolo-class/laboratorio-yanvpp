915cd0a74dc03533a2bc3c053c8769072e0f37de
Yan Vitor Pires Pereira <yanvitor.pp@gmail.com>
2025-07-08 21:03:08 -0300

    Treino

--- PROG1/aulas/Exercícios para treino - C/teste.c
+++ PROG1/aulas/Exercícios para treino - C/teste.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
-
-typedef struct {
-    int idade;
-    float altura;
-} Pessoa;
-
-void celebrar_aniversario(Pessoa *p)
-{
-    (*p).idade++;
-}
+#include <stdlib.h>  // srand, rand
+#include <time.h>    // time
 
 int main()
 {
-    Pessoa prof = {40, 1.64};
-    printf("Idade antes: %d\n", prof.idade);
-    celebrar_aniversario(&prof);
-    printf("Idade depois: %d\n", prof.idade);
+    int dado1, dado2, soma, num_rolagens;
+    int hist[13] = {};
+
+    printf("Entre com o numero de rolagens: ");
+    scanf("%d", &num_rolagens);
+
+    srand(time(NULL));
+
+    for (int i = 1; i <= num_rolagens; i++) {
+        dado1 = rand() % 6 + 1;
+        dado2 = rand() % 6 + 1;
+        soma = dado1 + dado2;
+        hist[soma]++;
+    }
+
+    for (soma = 2; soma <= 12; soma++) {
+        printf("%2d : %5.2f%%\n", soma, (float)hist[soma] / num_rolagens * 100.0);
+    }
+
     return 0;
 }
